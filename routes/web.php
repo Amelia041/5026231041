7a3171d7e8f5b1e19ec88cef9009ebfe1e1c1c96
Amelia041 <ameliarus098@gmail.com>
2025-05-21 12:38:51 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
 //import java.io; ini kalo di java
 
 
@@ -20,6 +23,17 @@ Route::get('blog', function () {
 });
 
 Route::get('hello', [Link::class,'helloword']);
+
+//pertemuan 12
+Route::get('dosen', [Link::class,'index']);
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+//roote pegawaicontroller
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+//roote blog
+Route::get('/blog', [BlogController::class,'home']);
+Route::get('/blog/tentang', [BlogController::class,'tentang']);
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
 
 //file 1-ets
 Route::get('frontend', function () {
